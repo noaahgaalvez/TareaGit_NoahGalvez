1104a94748f532b581d5c99bef50f7b2e3febe5f
alumnociclo <alumnociclo@dptoinformatica.iesfuengirola1.net>
2023-03-28 21:49:36 +0200

    Se agrega el proyecto

---
+++ src/tareagit_noahgalvez/Tabla.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareagit_noahgalvez;
+
+/**
+ *
+ * @author alumnociclo
+ */
+public class Tabla {
+    private int numTabla;
+
+    public int getNumero() {
+        return numTabla;
+    }
+
+    public void setNumero(int numero) {
+        this.numTabla = numero;
+    }
+    
+    public Tabla (int numeroTabla){
+        this.numTabla = numeroTabla;
+    }
+    
+    public String pintarTabla(){
+        StringBuilder tabla= new StringBuilder();
+        for (int i=0; i<11; i++){
+            int resultado = this.numTabla*i;
+            tabla.append(this.numTabla+" * "+i+" = "+resultado+"\n");
+        }
+        return tabla.toString();
+    }
+}
---
+++ src/tareagit_noahgalvez/TareaGit_NoahGalvez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareagit_noahgalvez;
+
+/**
+ *
+ * @author alumnociclo
+ */
+public class TareaGit_NoahGalvez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
